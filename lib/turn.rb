e745aaaeb47fdd46cfa3d3c1fd02842e1a76f6f4
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 19:15:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,6 @@ def valid_move?(board, index)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   case board[index].to_s.strip
   when "X" || "O"
