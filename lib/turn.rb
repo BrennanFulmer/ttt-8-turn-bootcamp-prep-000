8835e8d4b7e37a7ac7b79ba553788bcfa3f6ba5d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 19:17:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,12 @@
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board, index)
     true
@@ -16,13 +24,7 @@ def position_taken?(board, index)
   end
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
 
 def input_to_index(num)
   num.to_i - 1
