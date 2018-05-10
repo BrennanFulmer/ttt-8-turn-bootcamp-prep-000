5a63be2c86c2adf9255d5140b3a76f8338a13709
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 19:16:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,11 @@ def position_taken?(board, index)
     true
   end
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
